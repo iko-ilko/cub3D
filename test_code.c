943e7828230629f189d869c1948305ff33aaf188
malpizyed <dlftjdgg@gmail.com>
2024-01-09 20:36:00 +0900

    temp: already new branch

--- test_code.c
+++ test_code.c
@@ -5,10 +5,6 @@ void	map_info_print(t_map_info *map_info)
 	char	**map = map_info->map;
 
 	printf("----------------------------------------\n");
-	printf("north texture pointer: %p\n", map_info->north);
-	printf("south texture pointer: %p\n", map_info->south);
-	printf("west texture pointer: %p\n", map_info->west);
-	printf("east texture pointer: %p\n", map_info->east);
 	printf("ceiling RED: %d GREEN: %d BLUE: %d\n", map_info->ceiling_color / 256 / 256 % 256, map_info->ceiling_color / 256 % 256, map_info->ceiling_color % 256);
 	printf("floor RED: %d GREEN: %d BLUE: %d\n", map_info->floor_color / 256 / 256 % 256, map_info->floor_color / 256 % 256, map_info->floor_color % 256);
 	int i;
