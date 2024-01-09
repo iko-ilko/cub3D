a6c16cc92aa88a78a171bcc3bf516b7084479553
malpizyed <dlftjdgg@gmail.com>
2024-01-09 20:43:17 +0900

    create parsing branch

--- test_code.c
+++ test_code.c
@@ -5,6 +5,10 @@ void	map_info_print(t_map_info *map_info)
 	char	**map = map_info->map;
 
 	printf("----------------------------------------\n");
+	printf("north texture pointer: %p\n", map_info->image[NORTH].img);
+	printf("south texture pointer: %p\n", map_info->image[SOUTH].img);
+	printf("west texture pointer: %p\n", map_info->image[WEST].img);
+	printf("east texture pointer: %p\n", map_info->image[EAST].img);
 	printf("ceiling RED: %d GREEN: %d BLUE: %d\n", map_info->ceiling_color / 256 / 256 % 256, map_info->ceiling_color / 256 % 256, map_info->ceiling_color % 256);
 	printf("floor RED: %d GREEN: %d BLUE: %d\n", map_info->floor_color / 256 / 256 % 256, map_info->floor_color / 256 % 256, map_info->floor_color % 256);
 	int i;
