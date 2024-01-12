fb12e010e0c25e63cb7eabc52a4425a93245f358
seongwol <gbssw13@gmail.com>
2024-01-12 22:48:04 +0900

    fix: fixed my_mlx_pixel_put condition

--- src/draw/utils.c
+++ src/draw/utils.c
@@ -67,7 +67,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	if (x < 0 || y < 0 || x >= data->x_max - 1 || y >= data->y_max - 1)
+	if (x < 0 || y < 0 || x >= WIN_HOR - 1 || y >= WIN_VER - 1)
 		return ;
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
