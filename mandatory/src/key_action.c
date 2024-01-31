779b3125248fd25fbcf1780a5d9eeaafdd6a4eb6
iko-ilko <dlftjdgg@gmail.com>
2024-01-31 16:25:14 +0900

    add: set_corrected_pos() + temp gap version

--- mandatory/src/key_action.c
+++ mandatory/src/key_action.c
@@ -59,88 +59,149 @@ int	calculate_distance(t_vector pos, t_vector diff, int i)
 	printf("diff x %f diff y%f\n", diff.x, diff.y);
 	if (i == NORTH)
 	{
-		if (ft_abs(pos.y + diff.y - floor(pos.y)) < 0.137)
+		if (ft_abs(pos.y + diff.y - floor(pos.y)) < 0.1)
 			return (-1);
 	}
 	if (i == SOUTH)
 	{
 		printf("---%f\n", 1 - pos.y + diff.y - floor(pos.y));
-		if (ft_abs(pos.y + diff.y - ceil(pos.y)) < 0.137)
+		if (ft_abs(pos.y + diff.y - ceil(pos.y)) < 0.1)
 			return (-1);
 	}
 	if (i == EAST)
 	{
-		if (ft_abs(pos.x + diff.x - ceil(pos.x)) < 0.137)
+		if (ft_abs(pos.x + diff.x - ceil(pos.x)) < 0.1)
 			return (-1);
 	}
 	if (i == WEST)
 	{
-		if (ft_abs(pos.x + diff.x - floor(pos.x)) < 0.137)
+		if (ft_abs(pos.x + diff.x - floor(pos.x)) < 0.1)
 			return (-1);
 	}
 	return (1);
 }
-void	set_corrected_pos(t_data *d, t_vector diff, int move_i[4], int f)
-{
-	printf("f: %d\n", f);
-	if (f == X + Y + 1)
+double	set_corrected_pos(t_data *d, t_vector diff, int move_i[4], int flag)
+{(void)diff;
+	if (move_i[NORTH] == 0 && flag == Y)
 	{
-		d->point.pos.x += diff.x;
-		d->point.pos.y += diff.y;
+		printf("north\n");
+		return (floor(d->point.pos.y) + 0.1);
 	}
-	else if (f == Y)
+	if (move_i[SOUTH] == 0 && flag == Y)
 	{
-		printf("Y\n");
-		if (move_i[NORTH] == 0)
-			d->point.pos.y = floor(d->point.pos.y + diff.y) + 0.1;
-		else if (move_i[SOUTH] == 0)
-			d->point.pos.y = ceil(d->point.pos.y + diff.y) - 0.1;
-		d->point.pos.x += diff.x;
+		printf("south\n");
+		return (ceil(d->point.pos.y) - 0.1);
 	}
-	else if (f == X)
+	if (move_i[WEST] == 0 && flag == X)
 	{
-		printf("X\n");
-		if (move_i[WEST] == 0)
-			d->point.pos.x = floor(d->point.pos.x + diff.x) + 0.1;
-		else if (move_i[EAST] == 0)
-			d->point.pos.x = ceil(d->point.pos.x + diff.x) - 0.1;
-		d->point.pos.y += diff.y;
+		printf("west\n");
+		return (floor(d->point.pos.x) + 0.1);
 	}
+	if (move_i[EAST] == 0 && flag == X)
+	{
+		printf("east\n");
+		return (ceil(d->point.pos.x) - 0.1);
+	}
+	printf("dir x :%f dir y :%f\n", d->point.dir.x, d->point.dir.y);
+	if (flag == Y + 3)
+	{
+		if (d->point.dir.y < 0)
+			return (floor(d->point.pos.y) + 0.1);
+		else
+			return (ceil(d->point.pos.y) - 0.1);
+
+	}
+	if (flag == X + 3)
+	{
+		if (d->point.dir.x > 0)
+			return (floor(d->point.pos.x) + 0.1);
+		else
+			return (ceil(d->point.pos.x) - 0.1);
+	}
+
+
+	return (0);
+	// printf("f: %d\n", f);
+	// if (f == X + Y + 1)
+	// {
+	// 	d->point.pos.x += diff.x;
+	// 	d->point.pos.y += diff.y;
+	// }
+	// else if (f == Y)
+	// {
+	// 	printf("Y\n");
+	// 	if (move_i[NORTH] == 0)
+	// 		d->point.pos.y = floor(d->point.pos.y + diff.y) + 0.1;
+	// 	else if (move_i[SOUTH] == 0)
+	// 		d->point.pos.y = ceil(d->point.pos.y + diff.y) - 0.1;
+	// 	d->point.pos.x += diff.x;
+	// }
+	// else if (f == X)
+	// {
+	// 	printf("X\n");
+	// 	if (move_i[WEST] == 0)
+	// 		d->point.pos.x = floor(d->point.pos.x + diff.x) + 0.1;
+	// 	else if (move_i[EAST] == 0)
+	// 		d->point.pos.x = ceil(d->point.pos.x + diff.x) - 0.1;
+	// 	d->point.pos.y += diff.y;
+	// }
 }
 
 void	vector_move(t_data *data, t_point *point, int move_index[4], t_vector *diff)
 {
-	t_vector	pre_temp;
-	// t_vector	temp;
+	t_vector	temp;
 
-	pre_temp = vector_calculate(point->pos, *diff, PLUS);
-	if (move_index[NORTH] == 0 && diff->y < 0)
-		diff->y = 0;
-	else if (move_index[SOUTH] == 0 && diff->y >= 0)
-		diff->y = 0;
-	if (move_index[WEST] == 0 && diff->x < 0)
-		diff->x = 0;
-	else if (move_index[EAST] == 0 && diff->x >= 0)
-		diff->x = 0;
-	pre_temp.x += point->pos.x;
-	pre_temp.y += point->pos.y;
-	// temp = vector_calculate(point->pos, *diff, PLUS);
-	// (void)temp;
-	// set_corrected_pos(data, point, )
-	/*if (data->map[(int)temp.y][(int)temp.x] == '1')//둘 다 //앞 선 조건 딱 맞춰나눠떨어질 경우에 필요한걸까?
-				return;
-	else */if (data->map[(int)pre_temp.y][(int)pre_temp.x] == '0')
-		set_corrected_pos(data, *diff, move_index, X + Y + 1);
-	else if (data->map[(int)pre_temp.y][(int)point->pos.x] == '0')
-		set_corrected_pos(data, *diff, move_index, Y);
-	else if (data->map[(int)point->pos.y][(int)pre_temp.x] == '0')
-		set_corrected_pos(data, *diff, move_index, X);
-	// else if (data->map[(int)temp.y][(int)point->pos.x] == '0')
-	// 	diff->x = get_corrected_pos(data->map, move_index);
-	// else if (data->map[(int)point->pos.y][(int)temp.x] == '0')
-	// 	diff->y = get_corrected_pos(data->map, move_index);
-	// point->pos.y += diff->y;//else 안해도될듯? 어차피 못움직이면 0일테니
-	// point->pos.x += diff->x;
+	// X축 이동
+	temp = vector_calculate(point->pos, *diff, PLUS);
+	if (diff->x != 0) {
+		if (data->map[(int)point->pos.y][(int)temp.x] == '1' || move_index[WEST] == 0 || move_index[EAST] == 0) // X축 이동이 벽에 부딪히는 경우
+		{
+				printf("?\n");
+			if (diff->x < 0 && move_index[WEST] == 0)
+			{
+				point->pos.x = set_corrected_pos(data, *diff, move_index, X);
+			}
+			else if (diff->x >= 0 && move_index[EAST] == 0)
+			{
+				point->pos.x = set_corrected_pos(data, *diff, move_index, X);
+			}
+			else if (data->map[(int)point->pos.y][(int)temp.x] == '1')
+			{printf("3\n");
+				point->pos.x = set_corrected_pos(data, *diff, move_index, X + 3);
+				printf("ccccc:%f\n", point->pos.x);
+			}
+		}
+		else
+		{
+			point->pos.x += diff->x;
+		}
+	}
+	
+	// Y축 이동
+	temp = vector_calculate(point->pos, *diff, PLUS);
+	if (diff->y != 0) {
+		if (data->map[(int)temp.y][(int)point->pos.x] == '1' || move_index[NORTH] == 0 || move_index[SOUTH] == 0) // Y축 이동이 벽에 부딪히는 경우
+		{
+				printf("???\n");
+			if (diff->y < 0 && move_index[NORTH] == 0)
+			{printf("1\n");
+				point->pos.y = set_corrected_pos(data, *diff, move_index, Y);
+			}
+			else if (diff->y >= 0 && move_index[SOUTH] == 0)
+			{printf("2\n");
+				point->pos.y = set_corrected_pos(data, *diff, move_index, Y);
+			}
+			else if (data->map[(int)temp.y][(int)point->pos.x] == '1')
+			{printf("3\n");
+				point->pos.y = set_corrected_pos(data, *diff, move_index, Y + 3);
+				printf("ccccc:%f\n", point->pos.y);
+			}
+		}
+		else
+		{
+			point->pos.y += diff->y;
+		}
+	}
 }
 
 void	player_move(t_data *data, int key)
